b6a46b1ada6ed42ac3e3eea74c0ac7d779abf518
96nmvv9mc6-glitch <96nmvv9mc6@privaterelay.appleid.com>
2026-08-09 20:47:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bucket-list-v1';
+const CACHE = 'bucket-list-v2';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', (e)=>{
